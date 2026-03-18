cb5c919da04dbe7f096437c06bd4a2b1fb47c908
hm5821-sketch <hm5821@srmist.edu.in>
2026-03-18 10:06:37 +0530

    added UC4

--- HelloApp.java
+++ HelloApp.java
@@ -1,19 +1,22 @@
 public class HelloApp {
 public static void main (String[] args) {
-<<<<<<< HEAD
-String name;
-if (args.length > 0) {
-	name = args[0];
-} else {
-	name = "World";
-}
-System.out.println("Hello," + name + "!");
-=======
-String name = "World" ;
-if (args.length > 0) {
-	name = args[0];
-} 
-System.out.println("Hello, " + name + "!" );
->>>>>>> feature/UC3-display-name-default
+	String name;
+
+	if (args.length == 0)
+	{
+		System.out.println("Hello,World!");
+	} else {
+StringBuilder nameBuilder = new StringBuilder();
+for (int i = 0; i < args.length ; i++)  {
+	nameBuilder.append(args[i]);
+	if (i < args.length - 1) {
+		nameBuilder.append(", ");
+	}
+             }
+			 name = nameBuilder.toString();
+			 System.out.println("Hello," + name + "!" );
+			
+ }
+ 
 }
 }
