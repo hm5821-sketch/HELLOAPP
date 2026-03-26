bc8b424fbb912bbea661bf84c0f325ae00c6a75a
hm5821-sketch <hm5821@srmist.edu.in>
2026-03-26 10:05:18 +0530

    added UC7

--- HelloApp.java
+++ HelloApp.java
@@ -2,26 +2,11 @@ public class HelloApp {
 public static void main (String[] args) {
 
 
-	if (args.length == 0)
-	{
-		System.out.println("Hello,World!");
-	} else {
-StringBuilder nameBuilder = new StringBuilder();
-
-for ( String name : args)  {
-	
-	
-		nameBuilder.append(name).append(",");
+	String names = "World";
+	if (args.length > 0) {
+		names = String.join(",",args);
 	}
-	String namesList = nameBuilder.toString();
-	if(namesList.length() > 0) {
-		namesList = namesList.substring(0,namesList.length() - 2);
-	}
-             
-			 
-			 System.out.println("Hello," + nameBuilder.toString()  + "!" );
-			
- }
+	System.out.println("Hello,"+ names +"!");
  
 }
 }
